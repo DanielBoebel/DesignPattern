2d3fecd83e161b27696be381afd7677ea192c9c1
Daniel Boebel <daniel.boebel@gmail.com>
2019-12-17 10:04:13 +0100

    Initial commit

---
+++ DesignPatternExam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DesignPatternExam.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
